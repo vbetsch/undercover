bc6e436fd143ff166ff20ae2e1266cf1a55ee1f6
vbetsch <vic.betsch@gmail.com>
2023-01-23 17:43:08 +0100

    :boom: Remake JSON system

--- undercover.py
+++ undercover.py
@@ -1,32 +1,56 @@
-from json import load
+from json import load, dump
 from random import choice
 
+# --------------------------- JSON ---------------------------
+words_file = open('words.json', 'r')
+WORDS = load(words_file)
 
-# JSON
-rules_file = open('rules.json', 'r')
-RULES = load(rules_file)
+DATA = {
+    "PLAYERS": {
+        "WHITE_PLAYERS": [],
+        "UNDERCOVER_PLAYERS": [],
+        "CIVIL_PLAYERS": [],
+        "ROLE_PLAYERS": {}
+    },
+    "ROLES": {
+        "white": 0,
+        "undercover": 0,
+        "civil": 0
+    }
+}
 
+PLAYERS = DATA["PLAYERS"]["ROLE_PLAYERS"]
+WHITE_PLAYERS = DATA["PLAYERS"]["WHITE_PLAYERS"]
+UNDERCOVER_PLAYERS = DATA["PLAYERS"]["UNDERCOVER_PLAYERS"]
+CIVIL_PLAYERS = DATA["PLAYERS"]["CIVIL_PLAYERS"]
+ROLES = DATA["ROLES"]
 
-# Rules
-PLAYERS = RULES["PLAYERS"]
-ROLES = RULES["ROLES"]
-WORDS = RULES["WORDS"]
+# --------------------------- PLAYERS ---------------------------
+sum_players = 5
+# sum_players = int(input("Players : "))
 
+for index in range(1, sum_players + 1):
+    PLAYERS[f"Player{index}"] = None
 
-# Test rules
-sum_players = 0
-for player in PLAYERS:
-    sum_players += 1
+# --------------------------- ROLES COUNT ---------------------------
+whites = 1
+undercovers = 1
+civils = 3
+# whites = int(input("White : "))
+# undercovers = int(input("Undercover : "))
+# civils = int(input("Civil : "))
 
-sum_roles = 0
-for role in ROLES:
-    sum_roles += ROLES[role]
+roles = [whites, undercovers, civils]
 
-if sum_players != sum_roles:
+if sum_players != sum(roles):
     raise Exception("[ERROR] Sum of players and sum of roles are different")
 
 
-# Affect roles to players
+ROLES["white"] = whites
+ROLES["undercover"] = undercovers
+ROLES["civil"] = civils
+
+# --------------------------- ROLES AFFECTATION ---------------------------
 remaining_roles = []
 for role in ROLES:
     for index in range(1, ROLES[role] + 1):
@@ -39,10 +63,6 @@ for player in PLAYERS:
 
 print(PLAYERS)
 
-WHITE_PLAYERS = []
-UNDERCOVER_PLAYERS = []
-CIVIL_PLAYERS = []
-
 for player in PLAYERS:
     match PLAYERS[player]:
         case "white":
@@ -54,6 +74,8 @@ for player in PLAYERS:
         case _:
             raise Exception("[ERROR] Someone has nothing role")
 
-
+# --------------------------- GAME ---------------------------
 print(choice(WORDS))
- 
+
+with open('rules.json', 'w', encoding='utf-8') as file:
+    dump(DATA, file, ensure_ascii=False, indent=4)
