453cb98865894a1a3520fdbd336a29c0ad88b6cd
vbetsch <vic.betsch@gmail.com>
2023-02-01 01:11:24 +0100

    :white_check_mark: Update test_game

--- src/test.py
+++ src/test.py
@@ -4,6 +4,8 @@ from src.core.Game import Game
 def test_game():
     with Game() as game:
         game.sum_players = 5
+        for index in range(1, game.sum_players + 1):
+            game.players[f"Player{index}"] = None
         game.run()
 
 
