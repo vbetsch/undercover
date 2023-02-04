9d1256592a61c0b9cb7a6937d2a8d72860b9b288
vbetsch <vic.betsch@gmail.com>
2023-02-04 18:05:18 +0100

    :bug: Fix starting

--- src/main.py
+++ src/main.py
@@ -3,16 +3,10 @@
 from src.core.Game import Game
 
 
-def init():
-    # Service().clear_games()
-    pass
-
-
 def main():
     with Game() as game:
         game.run()
 
 
 if __name__ == '__main__':
-    init()
     main()
