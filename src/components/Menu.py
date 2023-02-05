5481aeac837f7dd8a9226b6b77f2342554f3dfda
vbetsch <vic.betsch@gmail.com>
2023-02-05 20:02:20 +0100

    :sparkles: Add options to Menu

--- src/components/Menu.py
+++ src/components/Menu.py
@@ -1,10 +1,35 @@
+from src.core.Inspector import Inspector
+from src.core.Interactor import Interactor
+
+
 class Menu:
-    def __init__(self, title, *options):
-        self.title = title
-        self.options = options
+    def __init__(self, mode, title, *entries):
+        self.mode = mode
+        self.title = title.upper()
+        self.options = {}
+        self.entries = list(entries)
+        self.compute_options()
+
+    def compute_options(self):
+        match self.mode:
+            case "first_letters":
+                for entry in self.entries:
+                    self.options[entry[0].lower()] = entry.capitalize()
+            case "numbers":
+                for index in range(0, len(self.entries)):
+                    self.options[index + 1] = self.entries[index].capitalize()
+            case _:
+                Inspector().exception(Interactor().trad("menu", "_menu_mode_not_found"))
 
     def show(self):
-        result = f"--------------- {self.title.upper()} ---------------\n"
+        result = f"--------------- {self.title} ---------------\n"
         for opt in self.options:
-            result += f"({opt[0].lower()}) {opt[0]}\n"
+            result += f"({opt}) {self.options[opt]}\n"
         print(result)
+
+    def add_options(self, *entries):
+        self.entries.extend(list(entries))
+        self.compute_options()
+
+    # TODO: def update_option(self, index, option)
+    # TODO: def insert_option(self, position, option)
--- src/test.py
+++ src/test.py
@@ -1,4 +1,7 @@
 from src.components.Menu import Menu
 
-menu = Menu("test", "ajouter un client", "modifier un client", "supprimer un client")
+menu = Menu("numbers", "test", "ajouter un client", "modifier un client", "supprimer un client")
+menu.show()
+
+menu.add_options("ajouter un produit", "ajouter un article")
 menu.show()
