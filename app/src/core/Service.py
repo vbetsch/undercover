fbfed0add7fb40de85d7a4c7b6b5428f50d438e7
vbetsch <vic.betsch@gmail.com>
2023-02-23 00:23:51 +0100

    :truck: Move constants

--- app/src/core/Service.py
+++ app/src/core/Service.py
@@ -6,21 +6,15 @@ from app.src.core.Meta import Singleton
 class Service(metaclass=Singleton):
     def __init__(self):
         self.games = []
-        self.files = {
-            "config": 'config.json',
-            "default": 'data/default.json',
-            "rules": 'data/rules.json',
-            "words": 'data/words.json',
-            "games": 'data/games.json',
-            "langs": {
-                "fr": 'lang/fr.json',
-                "en": 'lang/en.json'
-            }
+        self.config_path = 'config.json'
+        self.config = self.__load(self.config_path)
+        self.words = self.__load(self.config["constants"]["sources"]["words"])
+        self.rules = self.__load(self.config["constants"]["sources"]["default"])
+        self.langs = {
+            "fr": f"{self.config['lang_dir']}/fr.json",
+            "en": f"{self.config['lang_dir']}/en.json"
         }
-        self.config = self.__load(self.files["config"])
-        self.words = self.__load(self.files["words"])
-        self.rules = self.__load(self.files["default"])
-        self.dict = self.__load(self.files["langs"][self.config["lang"]])
+        self.dict = self.__load(self.langs[self.config["lang"]])
 
     @staticmethod
     def __load(path):
@@ -33,13 +27,13 @@ class Service(metaclass=Singleton):
             json.dump(data, file, ensure_ascii=False, indent=4)
 
     def compute_config(self):
-        self.__dump(self.files["config"], self.config)
-        
+        self.__dump(self.config_path, self.config)
+
     def get_lang(self):
         return self.config["lang"]
 
     def update_lang(self, lang):
-        self.dict = self.__load(self.files["langs"][lang])
+        self.dict = self.__load(self.langs[lang])
 
     def update_last_game(self, value):
         self.config["last_game"] = value
@@ -53,10 +47,10 @@ class Service(metaclass=Singleton):
         return None
 
     def read_games(self):
-        self.games = self.__load(self.files["games"])
+        self.games = self.__load(self.config["constants"]["sources"]["games"])
 
     def compute_games(self):
-        self.__dump(self.files["games"], self.games)
+        self.__dump(self.config["constants"]["sources"]["games"], self.games)
 
     def clear_games(self):
         self.games = []
@@ -65,16 +59,16 @@ class Service(metaclass=Singleton):
         self.compute_games()
 
     def get_default(self):
-        return self.__load(self.files["default"])
+        return self.__load(self.config["constants"]["sources"]["default"])
 
     def read_rules(self):
-        self.rules = self.__load(self.files["rules"])
+        self.rules = self.__load(self.config["constants"]["sources"]["rules"])
 
     def compute_rules(self):
-        self.__dump(self.files["rules"], self.rules)
+        self.__dump(self.config["constants"]["sources"]["rules"], self.rules)
 
     def read_words(self):
-        self.words = self.__load(self.files["words"])
+        self.words = self.__load(self.config["constants"]["sources"]["words"])
 
     def compute_words(self):
-        self.__dump(self.files["words"], self.words)
+        self.__dump(self.config["constants"]["sources"]["words"], self.words)
