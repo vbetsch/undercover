ad27ccfd5bf3f75c3dad4cff73eb4c607f15dcd1
vbetsch <vic.betsch@gmail.com>
2023-02-23 01:15:19 +0100

    :art: Format constants

--- scripts/make.py
+++ scripts/make.py
@@ -1,6 +1,46 @@
 import json
 import os
 
+PREFIX = "OK"
+DATA_PATH = 'app/data'
+default = {
+    "PLAYERS": {
+        "WHITE_PLAYERS": [],
+        "UNDERCOVER_PLAYERS": [],
+        "CIVILIAN_PLAYERS": [],
+        "ROLE_PLAYERS": {}
+    },
+    "ROLES": {
+        "white": 0,
+        "undercover": 0,
+        "civilian": 0
+    }
+}
+words = [
+    [
+        "car",
+        "truck"
+    ],
+    [
+        "violin",
+        "guitar"
+    ],
+    [
+        "facebook",
+        "whatsapp"
+    ],
+    [
+        "youtube",
+        "tiktok"
+    ]
+]
+games = []
+name_data_dict = {
+    "default": default,
+    "words": words,
+    "games": games
+}
+
 
 def __dump(path, data):
     with open(path, 'w', encoding='utf-8') as file:
@@ -8,45 +48,6 @@ def __dump(path, data):
 
 
 def main():
-    PREFIX = "OK"
-    DATA_PATH = 'app/data'
-    default = {
-        "PLAYERS": {
-            "WHITE_PLAYERS"     : [],
-            "UNDERCOVER_PLAYERS": [],
-            "CIVILIAN_PLAYERS"  : [],
-            "ROLE_PLAYERS"      : {}
-        },
-        "ROLES"  : {
-            "white"     : 0,
-            "undercover": 0,
-            "civilian"  : 0
-        }
-    }
-    words = [
-        [
-            "car",
-            "truck"
-        ],
-        [
-            "violin",
-            "guitar"
-        ],
-        [
-            "facebook",
-            "whatsapp"
-        ],
-        [
-            "youtube",
-            "tiktok"
-        ]
-    ]
-    games = []
-    name_data_dict = {
-        "default": default,
-        "words": words,
-        "games": games
-    }
     if not os.path.exists(DATA_PATH):
         os.mkdir(DATA_PATH)
         print(f"{PREFIX} {DATA_PATH} created")
